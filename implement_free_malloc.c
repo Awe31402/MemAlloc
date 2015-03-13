41c240ac79249e4dcb7739f2949e43eb2bff4c04
edward.wang <edward.wang@insydesw.com>
2015-03-14 00:24:09 +0800

    Change memory address print format to ulong

--- implement_free_malloc.c
+++ implement_free_malloc.c
@@ -116,18 +116,18 @@ int main(int argc, char *argv[])
 	int *int_ptr;
 	double *double_ptr;
 
-	printf("brefore awe_malloc, sbrk(0) = %10p\n", sbrk(0));
+	printf("brefore awe_malloc, sbrk(0) = %lu\n", sbrk(0));
 	chr_ptr = (char *)awe_malloc(sizeof(char));
 	*chr_ptr = 'x';
-	printf("after allocate %lu bytes, chr_ptr = %10p, *chr_ptr= %c , sbrk(0) = %10p\n",
+	printf("after allocate %lu bytes, chr_ptr = %lu, *chr_ptr= %c , sbrk(0) = %lu\n",
 		sizeof(char),chr_ptr, *chr_ptr, (void *)sbrk(0));
 	int_ptr = (int *)awe_malloc(sizeof(int));
 	*int_ptr = 100;
-	printf("after allocate %lu bytes, int_ptr = %10p, *int_ptr = %d, sbrk(0) = %10p\n",
+	printf("after allocate %lu bytes, int_ptr = %lu, *int_ptr = %d, sbrk(0) = %lu\n",
 		sizeof(int), int_ptr, *int_ptr, (void *)sbrk(0));
 	double_ptr = (double *) awe_malloc(sizeof(double));
 	*double_ptr = 1000000;
-	printf("after allocate %lu bytes, double_ptr = %10p, *double_ptr = %lf, sbrk(0) = %10p\n", 
+	printf("after allocate %lu bytes, double_ptr = %lu, *double_ptr = %lf, sbrk(0) = %lu\n", 
 		sizeof(double), double_ptr, *double_ptr, (void *)sbrk(0));
 
 	awe_free((void *)double_ptr);
@@ -135,11 +135,11 @@ int main(int argc, char *argv[])
 	awe_free((void *)chr_ptr);
 	int_ptr = (int *)awe_malloc(sizeof(int));
 	*int_ptr = 200;
-	printf("After awe_free, after allocate %lu bytes, int_ptr = %10p, *int_ptr = %d, sbrk(0) = %10p\n",
+	printf("After awe_free, after allocate %lu bytes, int_ptr = %lu, *int_ptr = %d, sbrk(0) = %lu\n",
 		sizeof(int), int_ptr, *int_ptr, (void *)sbrk(0));
 	double_ptr = (double *)awe_malloc(sizeof(double));
 	*double_ptr = 5000000;
-	printf("After awe_free, after allocate %lu bytes, double_ptr = %10p, *double_ptr = %lf, sbrk(0) = %10p\n",
+	printf("After awe_free, after allocate %lu bytes, double_ptr = %lu, *double_ptr = %lf, sbrk(0) = %lu\n",
 		sizeof(double), double_ptr, *double_ptr, (void *)sbrk(0));
 	exit(EXIT_SUCCESS);
 }
